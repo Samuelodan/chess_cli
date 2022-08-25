1503dcacfb3ef9f03977770cded0c92b8094c13e
Samuel ODaniels <samodan195@gmail.com>
2022-08-26 00:07:08 +0100

    Write Board#create_squares

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative './square.rb'
+
 # board for the chess game
 class Board
   attr_accessor :squares
@@ -7,4 +9,15 @@ class Board
   def initialize
     @squares = nil
   end
+
+  def create_squares
+    sqs = Array.new(8) { Array.new(8) }
+    sqs.each_with_index do |row, row_idx|
+      row.each_with_index do |item, idx|
+        sqs[row_idx][idx] = Square.new
+      end
+    end
+    sqs
+  end
 end
+
