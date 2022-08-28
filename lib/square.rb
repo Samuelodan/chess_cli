ee32f990130c7d2ace85f85e229d58841facce17
Samuel ODaniels <samodan195@gmail.com>
2022-08-28 22:32:23 +0100

    Write Square#draw_square

--- lib/square.rb
+++ lib/square.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 # smart chess squares
 class Square
   attr_accessor :position, :piece, :color
@@ -8,6 +7,18 @@ class Square
     @position = nil
     @piece = nil
     @color = nil
+  end
+
+  def draw_square
+    ansi_sqr_color = color == :white ? "\e[48;2;240;217;181m" : "\e[48;2;181;136;99m"
+    ansi_piece = if piece.nil?
+                  "\e[30m    \e[0m"
+                elsif piece.color = :white
+                  "\e[30m #{piece.symbol}  \e[0m"
+                else
+                  "\e[30m #{piece.symbol}  \e[0m"
+                end
+    print ansi_sqr_color + ansi_piece
   end
 end
 
