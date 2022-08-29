a1ef9a2f2109a552ae56123e0cf5fd235579889e
Samuel ODaniels <samodan195@gmail.com>
2022-08-29 13:37:17 +0100

    style(Square): reduce displayed square length

--- lib/square.rb
+++ lib/square.rb
@@ -12,11 +12,11 @@ class Square
   def draw_square
     ansi_sqr_color = color == :white ? "\e[48;2;240;217;181m" : "\e[48;2;181;136;99m"
     ansi_piece = if piece.nil?
-                  "\e[30m    \e[0m"
+                  "\e[30m   \e[0m"
                 elsif piece.color = :white
-                  "\e[30m #{piece.symbol}  \e[0m"
+                  "\e[30m #{piece.symbol} \e[0m"
                 else
-                  "\e[30m #{piece.symbol}  \e[0m"
+                  "\e[30m #{piece.symbol} \e[0m"
                 end
     print ansi_sqr_color + ansi_piece
   end
