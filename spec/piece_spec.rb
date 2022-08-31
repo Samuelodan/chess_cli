b4f37a00dc2f285adad24b28c1a34f764dcf642d
Samuel ODaniels <samodan195@gmail.com>
2022-08-31 14:36:52 +0100

    test(Piece): Write tests for Piece#moved?

---
+++ spec/piece_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../lib/piece'
+
+RSpec.describe Piece do
+  subject(:piece) { described_class.new(color: 'white', letter: 'N') }
+
+  describe '#moved?' do
+    context 'when piece has not moved' do
+      it 'returns false' do
+        result = piece.moved?
+        expect(result).to be(false)
+      end
+    end
+    context 'when piece has moved' do
+      before do
+        piece.moved = true
+      end
+
+      it 'returns true' do
+        result = piece.moved?
+        expect(result).to be(true)
+      end
+    end
+  end
+end
+
