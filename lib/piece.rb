c4590bdb30d8c50264d9f9cb2641c66e57c757cb
Samuel ODaniels <samodan195@gmail.com>
2022-08-31 15:47:21 +0100

    feat(Piece): Replace color parameter with #determine_color

--- lib/piece.rb
+++ lib/piece.rb
@@ -4,10 +4,15 @@
 class Piece
   attr_accessor :color, :letter
 
-  def initialize(color:, letter:)
-    @color = color
+  def initialize(letter:)
+    @color = determine_color
     @letter = letter
     @moved = false
+  end
+
+  def determine_color
+    str_letter = letter.to_s
+    clr = str_letter == str_letter.downcase ? :black : :white
   end
 
   def moved?
