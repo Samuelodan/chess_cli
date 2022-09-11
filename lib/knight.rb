445bff9b4a7772527469549658ef576891d2d6d2
Samuel ODaniels <samodan195@gmail.com>
2022-09-11 01:24:52 +0100

    feat(knight): Write method to calculate move coordinates

--- lib/knight.rb
+++ lib/knight.rb
@@ -11,6 +11,16 @@ class Knight < Piece
     letter == 'n'
   end
 
+  def moves
+    mv_arr = []
+    directions.each do |dir|
+      x = @coordinate[0] + dir[0]
+      y = @coordinate[1] + dir[1]
+      mv_arr << [x, y]
+    end
+    mv_arr
+  end
+
   def directions
     [
       [-2, -1], [+2, -1], [-2, +1], [+2, +1],
