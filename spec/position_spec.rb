f2cbdb02ebb691596255cf8e9ab717910caacd51
Samuel ODaniels <samodan195@gmail.com>
2022-09-14 00:46:04 +0100

    test(position): Add example for #== equality method

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -4,5 +4,15 @@ require_relative '../lib/position'
 
 RSpec.describe Position do
   subject(:position) { described_class.new(file: 'a', rank: 1) }
+
+  describe '#==' do
+    context 'when position objects have same values' do
+      it 'returns true' do
+        pos1 = Position.new(file: 'a', rank: 1)
+        pos2 = Position.new(file: 'a', rank: 1)
+        expect(pos1).to eq(pos2)
+      end
+    end
+  end
 end
 
