4f40bb33e464bee3a16314ea1ddbff71b45e094e
Samuel ODaniels <samodan195@gmail.com>
2022-09-14 20:47:08 +0100

    test(position): Rename #== to #eql?

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -5,7 +5,7 @@ require_relative '../lib/position'
 RSpec.describe Position do
   subject(:position) { described_class.new(file: 'a', rank: 1) }
 
-  describe '#==' do
+  describe '#eql?' do
     context 'when position objects have same values' do
       it 'returns true' do
         pos1 = Position.new(file: 'a', rank: 1)
