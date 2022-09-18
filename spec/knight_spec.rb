f00338519678ac5d393486b4cacca0eefa9b7aa9
Samuel ODaniels <samodan195@gmail.com>
2022-09-18 19:07:30 +0100

    test(knight): Add another #valid_moves example

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -48,6 +48,22 @@ RSpec.describe Knight do
         expect(move_count).to eql(exp_count)
       end
     end
+
+    context 'for fen string 2' do
+      before do
+        fen_str2 = 'r1bqkb1r/pppppppp/1n1N4/5P2/1P6/2n5/PPP1P1PP/RNBQKB1R w KQkq - 0 1'
+        board.arrange_pieces_from_fen(fen_str2)
+      end
+
+      it 'c3 knight has 8 moves' do
+        pos = Position.for('c3')
+        piece = board.square_at_position(pos).piece
+        piece.update_position(pos)
+        move_count = piece.valid_moves(board: board).length
+        exp_count = 8
+        expect(move_count).to eql(exp_count)
+      end
+    end
   end
 end
 
