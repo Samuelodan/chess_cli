2f188ee0e9b30ee8662550a583f5cf4678850bef
Samuel ODaniels <samodan195@gmail.com>
2022-09-20 23:56:26 +0100

    test(bishop): Add first #valid_moves example

---
+++ spec/bishop_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative '../lib/rook'
+
+RSpec.describe Bishop do
+  let(:board) { Board.new }
+
+  describe '#valid_moves' do
+    context 'for fen string 1' do
+      before do
+        fen_str1 = 'rnbqk1nr/pppppp1p/7b/8/5p2/2B1P3/PPPP1PPP/RN1QKBNR w KQkq - 0 1'
+        board.arrange_pieces_from_fen(fen_str1)
+      end
+
+      it 'c3 bishop has 7 moves' do
+        pos = Position.for('c3')
+        piece = board.square_at_position(pos).piece
+        piece.update_position(pos)
+        piece.update_board(board)
+        move_count = piece.valid_moves.length
+        exp_count = 7
+        expect(move_count).to eql(exp_count)
+      end
+    end
+  end
+end
