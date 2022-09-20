a94b16734e1cdf2909585d6f52a915920bd615bb
Samuel ODaniels <samodan195@gmail.com>
2022-09-21 00:49:38 +0100

    feat(queen): Add #directions

--- lib/queen.rb
+++ lib/queen.rb
@@ -12,5 +12,14 @@ class Queen < Piece
   def self.handles?(letter)
     letter == 'q'
   end
+
+  def directions
+    [
+      %i[continue_up], %i[continue_right],
+      %i[continue_down], %i[continue_left],
+      %i[continue_top_right], %i[continue_bottom_right],
+      %i[continue_bottom_left], %i[continue_top_left]
+    ]
+  end
 end
 
