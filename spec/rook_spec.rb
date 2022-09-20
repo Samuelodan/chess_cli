8c2516959079b99cf05ceded2a69cf478bf4e2b0
Samuel ODaniels <samodan195@gmail.com>
2022-09-20 17:06:48 +0100

    test(rook): Add another #valid_moves example

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -41,6 +41,16 @@ RSpec.describe Rook do
         exp_count = 10
         expect(move_count).to eql(exp_count)
       end
+
+      it 'a8 rook has zero moves' do
+        pos = Position.for('a8')
+        piece = board.square_at_position(pos).piece
+        piece.update_position(pos)
+        piece.update_board(board)
+        move_count = piece.valid_moves.length
+        exp_count = 0
+        expect(move_count).to eql(exp_count)
+      end
     end
   end
 end
