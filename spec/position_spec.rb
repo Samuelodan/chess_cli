67a86f03c1156a494bdd7a3d5ef2516ae79f3157
Samuel ODaniels <samodan195@gmail.com>
2022-09-20 18:55:06 +0100

    test(position): Add first #top_right example

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -270,5 +270,16 @@ RSpec.describe Position do
       end
     end
   end
+
+  describe '#top_right' do
+    context 'when position is c4' do
+      it 'returns all top right diagonal positions' do
+        pos = Position.new(file: 'c', rank: 4)
+        expected_pos = Position.for(['d5', 'e6', 'f7', 'g8'])
+        new_pos = pos.top_right
+        expect(new_pos).to eql(expected_pos)
+      end
+    end
+  end
 end
 
