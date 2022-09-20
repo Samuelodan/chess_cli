6da7b530249a53e3a6041ba3ca5f7eeae4f1c247
Samuel ODaniels <samodan195@gmail.com>
2022-09-20 23:45:27 +0100

    feat(bishop): Require Board

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative './piece'
+require_relative './board'
 
 # bishop subclass
 class Bishop < Piece
@@ -9,6 +10,13 @@ class Bishop < Piece
 
   def self.handles?(letter)
     letter == 'b'
+  end
+
+  def directions
+    [
+      %i[continue_top_right], %i[continue_bottom_right],
+      %i[continue_bottom_left], %i[continue_top_left]
+    ]
   end
 end
 
