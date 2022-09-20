2ea0eae586142262b9aadd1a8cd8d0683927293a
Samuel ODaniels <samodan195@gmail.com>
2022-09-20 22:44:02 +0100

    test(position): Add another #continue_bottom_left example

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -347,6 +347,15 @@ RSpec.describe Position do
         expect(new_pos).to eql(expected_pos)
       end
     end
+
+    context 'when in bottom-most position' do
+      it 'returns an empty array' do
+        pos = Position.new(file: 'f', rank: 1)
+        expected_pos = []
+        new_pos = pos.continue_bottom_left
+        expect(new_pos).to eql(expected_pos)
+      end
+    end
   end
 end
 
