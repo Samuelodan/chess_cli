94f4d5da81309148ae075a478d39b885d9bf1e1d
Samuel ODaniels <samodan195@gmail.com>
2022-09-21 16:41:06 +0100

    test(king): Add another #valid_moves example

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -21,6 +21,16 @@ RSpec.describe King do
         exp_count = 7
         expect(move_count).to eql(exp_count)
       end
+
+      it 'a6 king has 3 moves' do
+        pos = Position.for('a6')
+        piece = board.square_at_position(pos).piece
+        piece.update_position(pos)
+        piece.update_board(board)
+        move_count = piece.valid_moves.length
+        exp_count = 3
+        expect(move_count).to eql(exp_count)
+      end
     end
   end
 end
