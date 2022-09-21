a95e3acc07da9cba2cc456d499c131be8ccdac22
Samuel ODaniels <samodan195@gmail.com>
2022-09-21 20:50:03 +0100

    test(pawn): Add another #valid_moves example

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -32,6 +32,16 @@ RSpec.describe Pawn do
         exp_count = 2
         expect(move_count).to eql(exp_count)
       end
+
+      it 'd2 pawn has 1 moves' do
+        pos = Position.for('d2')
+        piece = board.square_at_position(pos).piece
+        piece.update_position(pos)
+        piece.update_board(board)
+        move_count = piece.valid_moves.length
+        exp_count = 1
+        expect(move_count).to eql(exp_count)
+      end
     end
   end
 end
