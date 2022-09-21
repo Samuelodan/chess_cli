8ff3f35cc11feb122d80cc25264ba811d9cc961b
Samuel ODaniels <samodan195@gmail.com>
2022-09-21 15:40:37 +0100

    test(position): Add another #top_left example

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -456,6 +456,15 @@ RSpec.describe Position do
         expect(new_pos).to eql(expected_pos)
       end
     end
+
+    context 'when position is b3' do
+      it 'returns a4 position object' do
+        pos = Position.new(file: 'b', rank: 3)
+        expected_pos = Position.new(file: 'a', rank: 4)
+        new_pos = pos.top_left
+        expect(new_pos).to eql(expected_pos)
+      end
+    end
   end
 end
 
