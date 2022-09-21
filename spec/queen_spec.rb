8fdf2f9877fe62266a607d97353d629391a05988
Samuel ODaniels <samodan195@gmail.com>
2022-09-21 13:03:23 +0100

    test(queen): Add first #valid_moves example

---
+++ spec/queen_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative '../lib/queen'
+
+RSpec.describe Queen do
+  let(:board) { Board.new }
+
+  describe '#valid_moves' do
+    context 'for fen string 1' do
+      before do
+        fen_str1 = 'rnb1kbnr/pppppppp/6q1/8/3Q4/8/PPPPPPPP/RNB1KBNR w KQkq - 0 1'
+        board.arrange_pieces_from_fen(fen_str1)
+      end
+
+      it 'd4 queen has 19 moves' do
+        pos = Position.for('d4')
+        piece = board.square_at_position(pos).piece
+        piece.update_position(pos)
+        piece.update_board(board)
+        move_count = piece.valid_moves.length
+        exp_count = 19
+        expect(move_count).to eql(exp_count)
+      end
+    end
+  end
+end
