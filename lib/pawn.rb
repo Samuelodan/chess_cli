db2ea43b77fb8965b3a6e24575b0d6048d43f036
Samuel ODaniels <samodan195@gmail.com>
2022-09-22 15:56:51 +0100

    feat(pawn): Add method check for enemy on right of white pawn

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -5,6 +5,7 @@ require_relative './board'
 
 # pawn subclass
 class Pawn < Piece
+
   def symbol
     sym = color == :white ? "\u2659" : "\u265f"
   end
@@ -17,6 +18,16 @@ class Pawn < Piece
     return [ %i[up], %i[up up] ] unless moved?
 
     [ %i[up] ]
+  end
+
+  def enemy_at_top_right?
+    tr = @position.top_right
+    square_at_tr = board.square_at_position(tr)
+    return false unless square_at_tr
+    piece_at_tr = square_at_tr.piece
+    return false if piece_at_tr.nil? || piece_at_tr.color == color
+
+    true
   end
 end
 
