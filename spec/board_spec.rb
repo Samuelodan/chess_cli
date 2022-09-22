9999abc44d9d1e67582aa25f67e854bc6592c560
Samuel ODaniels <samodan195@gmail.com>
2022-09-22 23:35:53 +0100

    test(board) Add another #is_move_valid? example

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -20,6 +20,21 @@ RSpec.describe Board do
         expect(result).to be false
       end
     end
+
+    context "when destination can be found among piece's moves" do
+      before do
+        board.arrange_pieces
+        pc_moves = [Position.for('a3'), Position.for('a4'), Position.for('b3')]
+        dest_position = Position.for('b3')
+        board.instance_variable_set(:@sel_pc_moves, pc_moves)
+        board.instance_variable_set(:@destination_position, dest_position)
+      end
+
+      it 'returns true' do
+        result = board.is_move_valid?
+        expect(result).to be true
+      end
+    end
   end
 end
 
