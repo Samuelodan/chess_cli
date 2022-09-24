a0572fe8c3e8636ec4b3dc83decd14c397ebe120
Samuel ODaniels <samodan195@gmail.com>
2022-09-24 23:36:25 +0100

    test(square): Add first #draw_square example

---
+++ spec/square_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative '../lib/square'
+require_relative '../lib/piece'
+
+RSpec.describe Square do
+  let(:piece) { double }
+  let(:square) { described_class.new }
+
+  describe '#draw_square' do
+    context 'for black square and white knight' do
+      before do
+        square.color = :black
+        square.piece = piece
+        allow(piece).to receive(:symbol).and_return("\u2658")
+      end
+
+      specify do
+        expect do
+          square.draw_square
+        end.to output("\e[48;2;181;136;99m\e[30m #{piece.symbol} \e[0m").to_stdout
+      end
+    end
+  end
+end
+
