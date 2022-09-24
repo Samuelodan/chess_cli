3a4226ea070a238f799dd75f41f078c871777956
Samuel ODaniels <samodan195@gmail.com>
2022-09-24 23:42:04 +0100

    test(square): Add another #draw_square example

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -21,6 +21,20 @@ RSpec.describe Square do
         end.to output("\e[48;2;181;136;99m\e[30m #{piece.symbol} \e[0m").to_stdout
       end
     end
+
+    context 'for black square and black bishop' do
+      before do
+        square.color = :black
+        square.piece = piece
+        allow(piece).to receive(:symbol).and_return("\u265d")
+      end
+
+      specify do
+        expect do
+          square.draw_square
+        end.to output("\e[48;2;181;136;99m\e[30m #{piece.symbol} \e[0m").to_stdout
+      end
+    end
   end
 end
 
