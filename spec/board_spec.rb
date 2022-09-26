e469ffbd9cf7a9dbee9582d83a3814baee3acf87
Samuel ODaniels <samodan195@gmail.com>
2022-09-26 22:57:46 +0100

    test(board): Add first #promote_pawn example

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -195,5 +195,21 @@ RSpec.describe Board do
       end
     end
   end
+
+  describe '#promote_pawn' do
+    context 'for queening a black pawn at h1' do
+      before do
+        fen_str = 'rnbqkb1r/1p3pPp/2p2p2/1pR5/N2p3n/2PR4/PPPPP1P1/1NBQKB1p w kq - 0 1'
+        board.arrange_pieces_from_fen(fen_str)
+      end
+
+      it 'replaces it with a black queen' do
+        square = board.square_at_position(Position.for('h1'))
+        board.promote_pawn
+        exp_pc_letter = 'q'
+        expect(square.piece.letter).to eql(exp_pc_letter)
+      end
+    end
+  end
 end
 
