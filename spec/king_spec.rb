9e5cc7e6361c2cebbf9f0b5efaf2b22f52339330
Samuel ODaniels <samodan195@gmail.com>
2022-09-27 22:29:39 +0100

    test(king): Add first #attack_moves example

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -78,5 +78,24 @@ RSpec.describe King do
       end
     end
   end
+
+  describe '#attack_moves' do
+    context 'for fen string 1' do
+      before do
+        fen_str1 = 'rnbq1bnr/ppp3p1/8/kP1p1p2/3pKPp1/8/P1PPP1PP/RNBQ1BNR w HAha - 0 1'
+        board.arrange_pieces_from_fen(fen_str1)
+      end
+
+      it 'e4 king has 3 attack moves' do
+        pos = Position.for('e4')
+        piece = board.square_at_position(pos).piece
+        piece.update_position(pos)
+        piece.update_board(board)
+        move_count = piece.attack_moves.length
+        exp_count = 3
+        expect(move_count).to eql(exp_count)
+      end
+    end
+  end
 end
 
