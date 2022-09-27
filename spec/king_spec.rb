2b159f8352ba5e91e76d74182189b7be9ff4ed25
Samuel ODaniels <samodan195@gmail.com>
2022-09-27 22:29:55 +0100

    test(king): Add another #attack_moves example

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -95,6 +95,16 @@ RSpec.describe King do
         exp_count = 3
         expect(move_count).to eql(exp_count)
       end
+
+      it 'a5 king has 1 attack move' do
+        pos = Position.for('a5')
+        piece = board.square_at_position(pos).piece
+        piece.update_position(pos)
+        piece.update_board(board)
+        move_count = piece.attack_moves.length
+        exp_count = 1
+        expect(move_count).to eql(exp_count)
+      end
     end
   end
 end
