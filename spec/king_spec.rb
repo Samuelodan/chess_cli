11427ec35492e2ae77d9f58b0deb28ecdb42f786
Samuel ODaniels <samodan195@gmail.com>
2022-09-28 02:35:39 +0100

    test(king): Update all #valid_moves examples

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -15,8 +15,6 @@ RSpec.describe King do
       it 'f4 king has 7 moves' do
         pos = Position.for('f4')
         piece = board.square_at_position(pos).piece
-        piece.update_position(pos)
-        piece.update_board(board)
         move_count = piece.valid_moves.length
         exp_count = 7
         expect(move_count).to eql(exp_count)
@@ -25,8 +23,6 @@ RSpec.describe King do
       it 'a6 king has 3 moves' do
         pos = Position.for('a6')
         piece = board.square_at_position(pos).piece
-        piece.update_position(pos)
-        piece.update_board(board)
         move_count = piece.valid_moves.length
         exp_count = 3
         expect(move_count).to eql(exp_count)
@@ -42,8 +38,6 @@ RSpec.describe King do
       it 'f4 king has 6 moves' do
         pos = Position.for('f4')
         piece = board.square_at_position(pos).piece
-        piece.update_position(pos)
-        piece.update_board(board)
         move_count = piece.valid_moves.length
         exp_count = 6
         expect(move_count).to eql(exp_count)
@@ -52,8 +46,6 @@ RSpec.describe King do
       it 'b5 king has 8 moves' do
         pos = Position.for('b5')
         piece = board.square_at_position(pos).piece
-        piece.update_position(pos)
-        piece.update_board(board)
         move_count = piece.valid_moves.length
         exp_count = 8
         expect(move_count).to eql(exp_count)
