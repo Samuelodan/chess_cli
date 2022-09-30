2caa7b13f61df08e9db54019857a9370af0475de
Samuel ODaniels <samodan195@gmail.com>
2022-09-30 13:54:03 +0100

    feat(board): Add #all_white_legal_moves

--- lib/board.rb
+++ lib/board.rb
@@ -218,6 +218,17 @@ class Board
 
   private
 
+  def all_white_legal_moves
+    pieces = get_all_white_pieces
+    total_moves = []
+    pieces.each do |pc|
+      pc.legal_moves.each do |pos|
+        total_moves << pos
+      end
+    end
+    total_moves
+  end
+
   def pieces_under_attack
     pieces = get_all_pieces
     under_attack = []
