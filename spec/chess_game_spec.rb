a49e60a106e671bc8ab74b8d7c2fafd96094fd73
Samuel ODaniels <samodan195@gmail.com>
2022-10-01 18:40:08 +0100

    test(chess_game): Add first `#change_turn` example

---
+++ spec/chess_game_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative '../lib/chess_game.rb'
+
+RSpec.describe ChessGame do
+  subject(:chess_game) do
+    described_class.new(board: board,
+                        player1: player1,
+                        player2: player2)
+  end
+  let(:board) { Board.new }
+  let(:player1) { Player.new }
+  let(:player2) { Player.new }
+
+  describe '#change_turn' do
+    context 'when current player is player1' do
+      it 'changes @current_player to player2' do
+        expect do
+          chess_game.change_turn
+        end.to change { chess_game.current_player }.to(player2)
+      end
+    end
+  end
+end
+
