d4821b73312fffc7a897d78e315e6a3c85ef5973
Samuel ODaniels <samodan195@gmail.com>
2022-10-01 16:16:56 +0100

    feat(game): Add `@current_player` and `#change_turns`

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -12,6 +12,15 @@ class Game do
     @board = board
     @player1 = player1
     @player2 = player2
+    @current_player = player1
+  end
+
+  def change_turn
+    if current_player == player1
+      @current_player = player2
+    else
+      @current_player = player1
+    end
   end
 end
 
