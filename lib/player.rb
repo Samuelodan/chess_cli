03043198af2eb90d58e924e57ccb5f5d76c6abd4
Samuel ODaniels <samodan195@gmail.com>
2022-10-03 20:28:02 +0100

    feat(player): Add `@color` and `#set_color`

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
 class Player
-  attr_reader :name
+  attr_reader :name, :color
 
   def initialize
     @name = nil
+    @color = nil
   end
 
   def set_name(name)
     @name = name
+  end
+
+  def set_color(color)
+    @color = color
   end
 end
 
